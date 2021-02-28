f89beaa46c859b399409a82ffbf2d2be1cd0f14b
Luke Berzins <lukeberzins16@gmail.com>
2021-02-28 11:11:21 -0500

    adding basic theme context to app.js, creating the start of a new functional theme component under context dir

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from './components/Nav'
 import Home from './pages/home/Home'
 import Projects from './pages/projects/Projects'
 import Personal from './pages/personal/Personal'
+import Theme from './context/Theme'
 
+export const ThemeContext = React.createContext()
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(true)
+
+  function toggleTheme() {
+    setDarkTheme(prev => !prev)
+  }
+
   return (
     <Router>
+      <ThemeContext.Provider value={darkTheme} >
       <Nav />
         <Switch>
           <Route path="/projects">
-          <Projects />
+            <Projects />
           </Route>
           <Route path="/personal">
-          <Personal />
+            <Personal />
           </Route>
-        <Route path="/">
-          <Home />
+          <Route path="/">
+            <Home />
           </Route>
         </Switch>
+      </ThemeContext.Provider>
     </Router>
   );
 }
