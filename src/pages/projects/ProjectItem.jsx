b22078ac0b4add4dd7f9a0b92c0d99010122f393
Luke Berzins <lukeberzins16@gmail.com>
2021-03-02 16:15:47 -0500

    projects page further developed, further putting variables into own css

--- src/pages/projects/ProjectItem.jsx
+++ src/pages/projects/ProjectItem.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { useTheme, useCatTheme } from '../../ThemeContext'
+import './ProjectItem.scss'
 
 export default function ProjectItem(props) {
   const theme = useTheme()
   const cat = useCatTheme()
   return (
-    <div className="card-container">
-      <p>{props.name}</p>
-      <p>{props.description}</p>
+    <div className="project-item-body">
+      <div className={theme}>
+        <div className="desc-container">
+         <h4>{props.name}</h4>
+          <p>{props.description}</p>
+        </div>
+        <div className="project-card">
+        <div className="slider">
+        </div>
+      <div className="projectInfo">
+        <div>
+          {props.git ? <p>Github: {props.git}</p> : <p></p>}
+          {props.site ? <p>Live site:{props.site}</p> : <p></p>}
+        </div>
+      </div>
+      </div>
+    </div>
     </div>
   )
 }
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,34 +1,80 @@
 import React from "react"
 import ProjectItem from "./ProjectItem"
 import "./Projects.scss"
-import { useTheme, useCatTheme } from '../../ThemeContext'
+import { useTheme } from '../../ThemeContext'
 
 
 export default function Projects(props) {
   const theme = useTheme()
-  const cat = useCatTheme()
 
   return (
     <div className={theme}>
       <div className="projects-body">
-
-      <div className="projects-intro">
-        <h1>Luke's Projects</h1>
-      </div>
-      <div className="project-item-holder">
-      <ProjectItem 
-        className=""
-        name="Reflexion"
-        picture=""
-        description="Uses voice Recognition with Tensorflow and google's Teachable Machine"
-      />
-      <ProjectItem 
-        className=""
-        name="Interview Scheduler"
-        picture=""
-        description="Make an appoinment!"
-      />
-      </div>
+        <div className="projects-intro">
+          <h1>Luke's Projects</h1>
+        </div>
+        <div className="project-item-holder">
+        <ProjectItem 
+          className=""
+          name="Reflexion"
+          picture=""
+          description="Uses voice Recognition with Tensorflow and google's  Teachable Machine"
+          git=""
+          site=""
+          display=""
+          stack=""
+        />
+        <ProjectItem 
+          className=""
+          name="Jungle"
+          picture=""
+          description="Built on Rails"
+          git=""
+          site=""
+          display=""
+          stack=""
+        />
+         <ProjectItem 
+          className=""
+          name="Interview Scheduler"
+          picture=""
+          description="Make an appoinment!"
+          git=""
+          site=""
+          display=""
+          stack=""
+        />
+         <ProjectItem 
+          className=""
+          name="Sneek"
+          picture=""
+          description="Online Shoe Store"
+          git=""
+          site=""
+          display=""
+          stack=""
+        />
+        <ProjectItem 
+          className=""
+          name="Tweeter"
+          picture=""
+          description="Online Shoe Store"
+          git=""
+          site=""
+          display=""
+          stack=""
+        />
+        <ProjectItem 
+          className=""
+          name="TinyApp"
+          picture=""
+          description="Online Shoe Store"
+          git=""
+          site=""
+          display=""
+          stack=""
+        />
+        </div>
       </div>
       <div className="finalfill"></div>
 
