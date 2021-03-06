5571432db6a5154e0ee2e2d6bb5b9731c56c9f1e
Luke Berzins <lukeberzins16@gmail.com>
2021-03-06 12:11:08 -0500

    changes to my personal page story

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import './Home.scss'
 import { useTheme, useThemeUpdate, useCatTheme, useCatUpdate } from '../../ThemeContext'
+import {
+  NavLink, useHistory
+} from "react-router-dom";
 export default function Home(props) {
   const theme = useTheme()
   const toggleTheme = useThemeUpdate()
@@ -20,9 +23,13 @@ export default function Home(props) {
           {cat ? <img alt="good-looking-dude" src="kitty/kitty-main.jpeg"></img> : <img alt="good-looking-dude" src="me.jpeg"></img>}
         </div>
         <div className="personal-text">
-        {cat ? <p>Hello good friends. I'm fatcat and I like to eat. Everything Luke owns is mine and now this website is mine. You can still click around to see some projects and learn more about my servant.</p> : <p>Hello good friends. I'm Luke and I like learning new things and making cool stuff happen. This is my website where you can get an idea of who I am. See portfolio to see some of the stuff I've done, and see my personal page to learn more about me. I've added some options so you can personalize your experience on my personal website.</p>}
+        {cat ? <p>Hello good friends. I'm fatcat and I like to eat. Everything Luke owns is mine and now this website is mine. You can still click around to see some projects and learn more about my servant.</p> : <p>Hello good friends. I'm Luke and I like learning new things and making cool stuff happen. This is my website where you can get an idea of who I am. See portfolio to see some of the stuff I've done, and see my personal page to learn more about me. I've added some options so you can personalize your experience on my personal website. Click below or use the nav bar.</p>}
         </div>
-        
+      <div className="extra-links">
+        <h3><NavLink className="navLink" to="/">home</NavLink></h3>
+        <h3><NavLink className="navLink" to="/projects">projects</NavLink></h3>
+        <h3><NavLink className="navLink" to="/personal">personal</NavLink></h3>
+        </div>
       </div>
       <div className="finalfill"></div>
     </div>
--- src/pages/personal/Personal.jsx
+++ src/pages/personal/Personal.jsx
@@ -19,16 +19,17 @@ export default function Personal(props) {
           <h3>519-533-8637</h3>
           <h3><a href="https://github.com/Luke-Berzins">Luke's Github</a></h3>
         </div>
-          <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I started my coding journey a little bit late. The amount I've learned since I started has only fueled me to learn more and become better. I am confident that with enough time I will do some really cool things, and I hope you check in the future so I can continue to share them with you. Although I wish I had found my love for code earlier in life, I spent my time beforehand fairly well in the meantime. If you want to read about my experience before I started coding and know more about me as a person, read below.</h2>
+          <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I started my coding journey a little bit late. The amount I've learned since I started has only fueled me to learn more and become better. I am confident that with enough time I will do some really cool things, and I hope you check in the future so I can continue to share them with you. Although I wish I had found my love for code earlier in life, I spent my time beforehand fairly well in the meantime. I've driven trains for CN, sighted in artillery pieces by compass and climbed a mountain or two. If you want to read about my experience before I started coding and know more about me as a person, read below.</h2>
         <div className="blog-holder">
           <Blog 
           theme={theme}
           blogType={"normal"}
-          text=""
+          text="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Most recently, I have been working as a Regional Liaison Officer for the Canadian Armed Forces. This has been a fantastic role where I have liaisoned with the great people at the Office of the Fire Marshall and Emergency Management and various Public Health units across Ontario, in order to keep our various organitzations fully coordinated as we defeat the COVID-19 pandemic."
           />
           <Blog 
           theme={theme}
           blogType={"reverse"}
+          text="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I heard about this role because I am an Army Reservist, specifically Artillery. I have served with the 11th Field Artillery Regiment for three and a half years and recieved some of the best experiences of my life."
           />
         </div>
       </div>
