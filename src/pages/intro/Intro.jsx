866115c9821ff71ef8c77a81bdad2c49fed736ea
Luke Berzins <lukeberzins16@gmail.com>
2021-03-11 12:29:45 -0500

    adding directories for intro

---
+++ src/pages/intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Intro.scss'
+
+export default function Intro() {
+  return (
+    <div>
+      <img src="/ground.png" className="ground"></img>          
+      <img src="/ground.png" className="ground"></img>      
+      <img src="/ground.png" className="ground"></img>      
+          
+    </div>
+  )
+}
