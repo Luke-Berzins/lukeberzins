5b303a38a8eb17c9da41709f796336695291e10a
Luke Berzins <lukeberzins16@gmail.com>
2021-03-13 11:18:19 -0500

    functional parallax effect

--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -1,29 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Intro.scss'
 
 export default function Intro() {
+  const [offset, setOffset] = useState(0);
+  
+
+  useEffect(() => {
+    function handleScroll() {
+      setOffset(window.pageYOffset);
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+
   return (
     <div>
-    <div className="header-container">
+    <div className="intro-container">
       <h1>Luke Berzins</h1>
       <br></br>
       <br></br>
-    </div>
-    <header>
+    
 
     <div className="parallax-holder">
         
-        <img src='/intro/sky.png'  alt="intro-pic" className="sky"></img> 
-        <img src='/intro/background.png'  alt="intro-pic" className="background"></img>          
-        <img src="/intro/midground.png" alt="intro-pic2" className="midground"></img>      
-        <img src="/intro/foreground.png" alt="intro-pic2" className="foreground"></img>   
+        <img src='/intro/sky.png'  
+        alt="intro-pic" 
+        className="sky"
+        style={{
+          filter: `blur(4px)`,
+          transform: `translateY(${offset * 0.5}px)`
+        }}
+        ></img> 
+        
+        <img src='/intro/background.png'  
+        alt="intro-pic" 
+        className="background" 
+        style={{
+          transform: `translateY(${offset * 0.52}px)`
+        }}
+        ></img>          
+       
+        <img src="/intro/midground.png" 
+        alt="intro-pic2" 
+        className="midground" 
+        style={{
+          transform: `translateY(${offset * 0.54}px)`
+        }}
+        ></img>  
+
+        <img src="/intro/foreground.png" 
+        alt="intro-pic2" 
+        className="foreground"
+        ></img>   
                
     </div>
-    </header>
+    
 
     
     <section>
+      <div className="shadow">
+
+      </div>
     <div className="intro-content"> 
       <div className="intro-text">
           <h3 className="intro-title">Hi!</h3>
@@ -36,6 +78,7 @@ export default function Intro() {
     </div>
       </section>  
     
+    </div>
     </div>
   )
 }
